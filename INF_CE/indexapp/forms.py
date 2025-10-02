1c21b1565d0d1515581a45b24495ae45438ae97a
Shadia <shadisantiago2020@gmail.com>
2025-10-02 10:46:09 -0500

    se centro el titulo en cableoperadoresform.html

--- INF_CE/indexapp/forms.py
+++ INF_CE/indexapp/forms.py
@@ -100,4 +100,57 @@ class CableoperadoresModelForm(forms.ModelForm):
     class Meta:
         model = Cableoperadores
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nombre del cableoperador'
+            }),
+            'nombre_largo': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nombre largo del cableoperador'
+            }),
+            'NIT': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'NIT del cableoperador'
+            }),
+            'RegistroTic': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Registro TIC del cableoperador'
+            }),
+            'CodigoInterno': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Codigo Interno del cableoperador'
+            }), 
+            
+            'pais': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Pais del cableoperador'
+            }),
+            'ciudad': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ciudad del cableoperador'
+            }),
+            'direccion': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Dirección del cableoperador'
+            }),
+            'Representante': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Representante del cableoperador'
+            }),
+            'telefono': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Telefono del cableoperador'
+            }),
+            'correo': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Correo del cableoperador'
+            }),
+            'ejecutiva': forms.Select(attrs={
+                'class': 'form-DateInput',
+                'widget': Select2Widget
+                
+            }),
+        }
+        
         
