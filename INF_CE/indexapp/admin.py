bf58f7b28b093ccee8e48aedcab960eb605e5de8
adrian G <adriangomezcortes04@gmail.com>
2025-10-02 17:07:06 -0500

    prueba

--- INF_CE/indexapp/admin.py
+++ INF_CE/indexapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Cableoperadores
-
+from .models import Cableoperadores, Cable, Caja_empalme, Contratos, Nap, Usos, Reserva
 
 admin.site.register(Cableoperadores)
+admin.site.register(Cable)
+admin.site.register(Caja_empalme)
+admin.site.register(Contratos)
+admin.site.register(Nap)
+admin.site.register(Usos)
+admin.site.register(Reserva)
 # Unregister the User model from the admin site
 admin.site.unregister(User)
 # Admin 
